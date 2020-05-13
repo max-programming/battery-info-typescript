d7437854fa78d56588ec9b938b2cb7605a57510c
max-programming <ui.sabuwala@gmail.com>
2020-05-13 13:15:25 +0530

    Unsupported browsers message added

--- app.js
+++ app.js
@@ -6,33 +6,39 @@ let batteryLevel, isCharging, dischTime;
 
 // MAIN Function Start
 async function showBattery() {
-  const battery = await navigator.getBattery();
-  // console.log(battery);
-  batteryLevel = `${battery.level * 100}%`;
-  isCharging = battery.charging;
-  dischTime = battery.dischargingTime;
-  // Change Level
-  battery.addEventListener("levelchange", () => changeLevel(battery));
-  // Animate when charging
-  battery.addEventListener("chargingchange", () => changeChargingAnimation());
-  // Change Discharging Time
-  // battery.addEventListener("dischargingtimechange", () =>
-  //   showDischargingTime(dischTime)
-  // );
-  progressBar.setAttribute("aria-valuenow", battery.level);
-  progressBar.style.width = batteryLevel;
-  progressBar.innerText = batteryLevel;
-  // Change Level
-  changeLevel(battery);
-  // Change Battery color
-  progressBar.classList.add(changeBatteryColor(battery.level * 100));
-  // Change Discharging Time
-  // showDischargingTime(dischTime);
-  // console.log(isCharging);
-  // Display text "Charging..."
-  chargingTextDisplay(isCharging);
-  // Charging Animation
-  changeChargingAnimation();
+  try {
+    const battery = await navigator.getBattery();
+    // console.log(battery);
+    batteryLevel = `${battery.level * 100}%`;
+    isCharging = battery.charging;
+    dischTime = battery.dischargingTime;
+    // Change Level
+    battery.addEventListener("levelchange", () => changeLevel(battery));
+    // Animate when charging
+    battery.addEventListener("chargingchange", () => changeChargingAnimation());
+    // Change Discharging Time
+    // battery.addEventListener("dischargingtimechange", () =>
+    //   showDischargingTime(dischTime)
+    // );
+    progressBar.setAttribute("aria-valuenow", battery.level);
+    progressBar.style.width = batteryLevel;
+    progressBar.innerText = batteryLevel;
+    // Change Level
+    changeLevel(battery);
+    // Change Battery color
+    progressBar.classList.add(changeBatteryColor(battery.level * 100));
+    // Change Discharging Time
+    // showDischargingTime(dischTime);
+    // console.log(isCharging);
+    // Display text "Charging..."
+    chargingTextDisplay(isCharging);
+    // Charging Animation
+    changeChargingAnimation();
+  } catch (err) {
+    const errMessage = document.querySelector(".unsupported");
+    errMessage.style.display = "block";
+    document.querySelector(".supported").style.display = "none";
+  }
 }
 // MAIN Funtion End
 showBattery();
