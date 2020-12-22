0189234fd1c68f153aa50dd2b6a19e7bd8cdd683
max-programming <ui.sabuwala@gmail.com>
2020-12-22 10:19:43 +0530

    💥 Started using Typescript

---
+++ src/app.ts
@@ -0,0 +1,33 @@
+import { GetBattery } from "./types";
+import {
+  changeLevel,
+  changeChargingAnimation,
+  changeDoneIn,
+} from "./functions";
+
+const navigatorObj: any = navigator;
+let batteryLevel: string, isCharging: boolean;
+
+// MAIN Function Start
+async function showBattery() {
+  try {
+    const battery: GetBattery = await navigatorObj.getBattery();
+    isCharging = battery.charging;
+    // Change Level
+    batteryLevel = changeLevel(battery);
+    changeChargingAnimation(battery.charging);
+    changeDoneIn(battery.chargingTime, battery.dischargingTime);
+  } catch (err) {}
+}
+// MAIN Funtion End
+if ("getBattery" in navigator) {
+  showBattery();
+  setInterval(showBattery, 1000);
+} else {
+  const unsupportedDiv = document.querySelector(
+    ".unsupported"
+  ) as HTMLHeadingElement;
+  const supportedDiv = document.querySelector(".supported") as HTMLDivElement;
+  unsupportedDiv.style.display = "block";
+  supportedDiv.style.display = "none";
+}
